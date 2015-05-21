ed4139281bee205c6c79fe14587fd76b103249ec
Steve Bennett <stevage@gmail.com>
2015-05-21 22:16:57 +1000

    Remove header image

--- run_server.sh
+++ run_server.sh
@@ -2,5 +2,5 @@
 
 forever stopall
 sudo /etc/init.d/varnish restart
-nohup forever server.js > output.log 2> error.log < /dev/null &
+nohup forever server.js --port 3003 > output.log 2> error.log < /dev/null &
 
