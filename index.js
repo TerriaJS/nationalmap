f68f2ef1eb43daad43e4de8650c177e3d8e6f3b3
kgrochow <keith.grochow@nicta.com.au>
2015-06-11 11:55:50 +1000

    proxy path

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var configuration = {
     terriaBaseUrl: 'build/TerriaJS',
     cesiumBaseUrl: undefined, // use default
     bingMapsKey: undefined, // use Cesium key
-    proxyBaseUrl: 'proxy/',
+    proxyBaseUrl: '/proxy/',
     conversionServiceBaseUrl: 'convert',
     regionMappingDefinitionsUrl: 'data/regionMapping.json'
 };
