41fd0760de801e16a31c8bdf9b23fadd9c2d2473
Ashley Stacey <ashley.stacey@data61.csiro.au>
2017-03-21 14:36:04 +1100

    Update from Experimental to ExperimentalMenu so that it is a bit more descriptive.

--- lib/Views/UserInterface.jsx
+++ lib/Views/UserInterface.jsx
@@ -5,7 +5,7 @@ import version from '../../version';
 import StandardUserInterface from 'terriajs/lib/ReactViews/StandardUserInterface/StandardUserInterface.jsx';
 import MenuItem from 'terriajs/lib/ReactViews/StandardUserInterface/customizable/MenuItem';
 import RelatedMaps from './RelatedMaps';
-import { Menu, Nav, Experimental } from 'terriajs/lib/ReactViews/StandardUserInterface/customizable/Groups';
+import { Menu, Nav, ExperimentalMenu } from 'terriajs/lib/ReactViews/StandardUserInterface/customizable/Groups';
 import MeasureTool from 'terriajs/lib/ReactViews/Map/Navigation/MeasureTool';
 import AugmentedVirtualityTool from 'terriajs/lib/ReactViews/Map/Navigation/AugmentedVirtualityTool.jsx';
 
@@ -21,9 +21,9 @@ export default function UserInterface(props) {
             <Nav>
                 <MeasureTool terria={props.viewState.terria} key="measure-tool"/>
             </Nav>
-            <Experimental>
+            <ExperimentalMenu>
                 <AugmentedVirtualityTool viewState={props.viewState} terria={props.viewState.terria} experimentalWarning={true} key="augmented-virtuality-tool"/>
-            </Experimental>
+            </ExperimentalMenu>
         </StandardUserInterface>
     );
 }
