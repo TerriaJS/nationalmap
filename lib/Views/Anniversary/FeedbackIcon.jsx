46c29c5855fd956978830c661ec58db130280399
Wing Ho <mrwingh@gmail.com>
2019-07-24 16:30:37 +1000

    Formatting

--- lib/Views/Anniversary/FeedbackIcon.jsx
+++ lib/Views/Anniversary/FeedbackIcon.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const FeedbackIcon = props => (
+export const FeedbackIcon = props => (
   <svg width="1em" height="1em" viewBox="0 0 18 18" {...props}>
     <defs>
       <path
